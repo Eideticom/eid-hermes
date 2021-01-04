Eideticom/eid-hermes

commit fbc84da788503bd792bdf902f43e64daa0f9638a

driver: add support for program/data transfer
This patch introduces support for downloading an eBPF program to a
program slot and to transfer data to/from a data slot.

An eBPF program can be downloaded to the device through an ioctl,
passing the program and its length as the argp parameter of the ioctl.

Data slots are written/read through the write()/read() syscalls.

Every file descriptor of the Hermes char device will have associated to
it a program slot and data slot, which are allocated the first time we
issue the ioctl or write syscalls. Slots are currently allocated in
software but might later be done on hardware.

In the future, the write() syscall will also trigger the execution of
the corresponding program. For that reason, we disallow writes without
a previous ioctl.

If a user needs to run multiple programs, then the user should use
multiple file descriptors (using a second ioctl will overwrite the
current program slot). Note that this prevents effective "program
chaining", that is, using the unmodified output of a program as the
input of another.

Signed-off-by: Martin Oliveira <martin.oliveira@eideticom.com>

src/driver/hermes_cdev.c
@@ -28,7 +28,9 @@
 #include <linux/module.h>
 #include <linux/pci.h>
 
+#include "hermes_uapi.h"
 #include "hermes_mod.h"
+#include "xdma_sgdma.h"
 
 #define HERMES_MINOR_BASE	0
 #define HERMES_MINOR_COUNT	16
@@ -38,8 +40,186 @@ static struct class *hermes_class;
 DEFINE_IDA(hermes_ida);
 static dev_t hermes_devt;
 
+struct hermes_env {
+	struct hermes_dev *hermes;
+	int32_t prog_slot;
+	int32_t data_slot;
+};
+
+static inline void hermes_release_slot_prog(struct hermes_env *env)
+{
+	if (env->prog_slot >= 0)
+		ida_simple_remove(&env->hermes->prog_slots, env->prog_slot);
+}
+
+static inline void hermes_release_slot_data(struct hermes_env *env)
+{
+	if (env->data_slot >= 0)
+		ida_simple_remove(&env->hermes->data_slots, env->data_slot);
+}
+
+static inline int32_t hermes_request_slot_prog(struct hermes_dev *hermes)
+{
+	return ida_simple_get(&hermes->prog_slots, 0, hermes->cfg.ehpslot,
+			      GFP_KERNEL);
+}
+
+static inline int32_t hermes_request_slot_data(struct hermes_dev *hermes)
+{
+	return ida_simple_get(&hermes->data_slots, 0, hermes->cfg.ehdslot,
+			      GFP_KERNEL);
+}
+
+static int hermes_open(struct inode *inode, struct file *filp)
+{
+	struct hermes_dev *hermes;
+	struct hermes_env *env;
+
+	hermes = container_of(inode->i_cdev, struct hermes_dev, cdev);
+	env = kzalloc(sizeof(*env), GFP_KERNEL);
+	env->hermes = hermes;
+	env->prog_slot = -1;
+	env->data_slot = -1;
+	filp->private_data = env;
+
+	return 0;
+}
+
+static int hermes_close(struct inode *inode, struct file *filp)
+{
+	struct hermes_env *env = filp->private_data;
+
+	hermes_release_slot_prog(env);
+	hermes_release_slot_data(env);
+
+	kfree(env);
+
+	return 0;
+}
+
+static ssize_t hermes_read(struct file *filp, char __user *buff, size_t count,
+		loff_t *f_pos)
+{
+	struct hermes_env *env = filp->private_data;
+	struct hermes_pci_dev *hpdev = env->hermes->hpdev;
+	struct hermes_cfg *cfg = &hpdev->hdev->cfg;
+	struct xdma_channel *chnl;
+	long res;
+	loff_t pos;
+
+	if (env->data_slot < 0)
+		return -ENODATA;
+
+	if (count + *f_pos > cfg->ehdssze) {
+		count = cfg->ehdssze - *f_pos;
+		if ((int) count < 0)
+			count = 0;
+	}
+
+	chnl = xdma_get_c2h(hpdev);
+
+	pos = cfg->ehdsoff + *f_pos + env->data_slot * cfg->ehdssze;
+
+	res = xdma_channel_read_write(chnl, (char *) buff, count, &pos, false);
+
+	return res;
+}
+
+static ssize_t hermes_write(struct file *filp, const char __user *buff,
+		size_t count, loff_t *f_pos)
+{
+	struct hermes_env *env = filp->private_data;
+	struct hermes_pci_dev *hpdev = env->hermes->hpdev;
+	struct hermes_cfg *cfg = &hpdev->hdev->cfg;
+	struct xdma_channel *chnl;
+	long res;
+	loff_t pos;
+
+	if (env->prog_slot < 0) {
+		pr_err("Program has not been downloaded to device. Aborting.\n");
+		return -EBADFD;
+	}
+
+	if (count + *f_pos > cfg->ehdssze) {
+		pr_err("Tried to write beyond data slot size. Count: 0x%lx Offset: 0x%llx Slot size:0x%x\n",
+				count, *f_pos, cfg->ehdssze);
+		return -EINVAL;
+	}
+
+	if (env->data_slot < 0) {
+		env->data_slot = hermes_request_slot_data(hpdev->hdev);
+		if (env->data_slot < 0)
+			return env->data_slot;
+	}
+
+	chnl = xdma_get_h2c(hpdev);
+
+	pos = cfg->ehdsoff + *f_pos + env->data_slot * cfg->ehdssze;
+
+	res = xdma_channel_read_write(chnl, (char *) buff, count, &pos, true);
+
+	return res;
+}
+
+static long hermes_download_program(struct hermes_env *env,
+				    struct hermes_download_prog_ioctl_argp *argp)
+{
+	struct hermes_pci_dev *hpdev = env->hermes->hpdev;
+	struct hermes_cfg *cfg = &hpdev->hdev->cfg;
+	struct xdma_channel *chnl;
+	long res;
+	loff_t pos;
+
+	if (argp->flags)
+		return -EINVAL;
+
+	if (argp->len > cfg->ehpssze) {
+		pr_err("Program size greater than program slot size: 0x%x > 0x%x\n",
+				argp->len, cfg->ehpssze);
+		return -EINVAL;
+	}
+
+	if (env->prog_slot < 0) {
+		env->prog_slot = hermes_request_slot_prog(hpdev->hdev);
+		if (env->prog_slot < 0)
+			return env->prog_slot;
+	}
+
+	chnl = xdma_get_h2c(hpdev);
+
+	pos = cfg->ehpsoff + env->prog_slot * cfg->ehpssze;
+
+	res = xdma_channel_read_write(chnl, (char *) argp->prog, argp->len,
+				      &pos, true);
+
+	if (res < 0)
+		return res;
+	else if (res != argp->len)
+		return -EIO;
+
+	return 0;
+}
+
+static long hermes_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	struct hermes_env *env = filp->private_data;
+
+	switch(cmd) {
+	case HERMES_DOWNLOAD_PROG_IOCTL:
+		return hermes_download_program(env,
+				(struct hermes_download_prog_ioctl_argp *) arg);
+	default:
+		return -EINVAL;
+	}
+}
+
 static const struct file_operations hermes_fops = {
 	.owner = THIS_MODULE,
+	.open = hermes_open,
+	.release = hermes_close,
+	.unlocked_ioctl = hermes_ioctl,
+	.read = hermes_read,
+	.write = hermes_write,
 };
 
 static struct hermes_dev *to_hermes(struct device *dev)
@@ -108,6 +288,9 @@ int hermes_cdev_create(struct hermes_pci_dev *hpdev)
 	err = cdev_device_add(&hermes->cdev, &hermes->dev);
 	if (err)
 		goto out_ida;
+
+	ida_init(&hermes->prog_slots);
+	ida_init(&hermes->data_slots);
 
 	dev_info(&hermes->dev, "device created");
 

src/driver/hermes_mod.h
@@ -87,6 +87,8 @@ struct hermes_dev {
 	int id;
 
 	struct hermes_cfg cfg;
+	struct ida prog_slots;
+	struct ida data_slots;
 };
 
 /* XDMA PCIe device specific book-keeping */

src/include/hermes_uapi.h
@@ -1,0 +1,39 @@
+/*******************************************************************************
+ *
+ * Hermes eBPF-based PCIe Accelerator
+ * Copyright(c) 2020 Eideticom, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "LICENSE".
+ *
+ * Martin Oliveira <martin.oliveira@eideticom.com>
+ *
+ ******************************************************************************/
+
+#ifndef HERMES_H
+#define HERMES_H
+
+#include <linux/types.h>
+
+struct hermes_download_prog_ioctl_argp {
+	__u32 len;
+	__u64 prog;
+	__u32 flags;
+};
+
+#define HERMES_DOWNLOAD_PROG_IOCTL    _IOW('H', 0x50, \
+		struct hermes_download_prog_ioctl_argp)
+
+#endif // HERMES_H

commit 1a34bc830ad7a30132163b552f3c82f596651547

driver: destroy slots IDAs when destroying cdev
Fixes a memory leak

Fixes: fbc84da7 ("add support for program/data transfer")

src/driver/hermes_cdev.c
@@ -314,6 +314,8 @@ void hermes_cdev_destroy(struct hermes_pci_dev *hpdev)
 
 	cdev_device_del(&hermes->cdev, &hermes->dev);
 	ida_simple_remove(&hermes_ida, hermes->id);
+	ida_destroy(&hermes->prog_slots);
+	ida_destroy(&hermes->data_slots);
 	put_device(&hermes->dev);
 }
 